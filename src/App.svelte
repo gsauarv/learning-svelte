c57c2f83b3cd0bac020c943c8db67ac4569f3d98
gsauarv <gsaurav2000@gmail.com>
2022-02-24 16:56:48 +0545

    just started learning about the reactive statements and components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,41 @@
 <script>
-	export let name;
+  let firstName = "Saurav";
+  let beltColor = "black";
+  let lastName = "Ghimire";
+  //   reactive value
+  $: fullName = `${firstName} ${lastName}`;
+  //   reactivestatements
+  $: {
+    console.log(beltColor);
+    console.log(fullName);
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <p>{fullName} {beltColor} belt</p>
+  <input type="text" bind:value={firstName} />
+  <input type="text" bind:value={lastName} />
+  <input type="text" bind:value={beltColor} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
